764a24a4037ce6c8ad4039f165ca1fcddd553650
Jan Misar <misar.jan@gmail.com>
2018-03-25 19:45:02 +0200

    Initial commit

---
+++ EmojiKeyboard/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EmojiKeyboard
+//
+//  Created by Jan Misar on 25.03.18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
